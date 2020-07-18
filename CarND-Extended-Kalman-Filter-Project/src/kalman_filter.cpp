431eb718f45923f14f76eb5b8c7501105a4d95a2
huangchu <huangchu.umich.edu>
2020-07-18 19:58:44 -0400

    Update KalmanFilter

--- CarND-Extended-Kalman-Filter-Project/src/kalman_filter.cpp
+++ CarND-Extended-Kalman-Filter-Project/src/kalman_filter.cpp
@@ -24,18 +24,60 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 
 void KalmanFilter::Predict() {
   /**
-   * TODO: predict the state
+   * TODO: predict the state mean and cov
    */
+	// x_{k+1} = F * x_{k} + G * a_k.
+	// G * a_{k} has mean 0 and cov Q
+	// the initial transition matrix F_
+	x_ = F_ * x_;
+	P_ = F_ * P_ * (F_.transpose()) + Q_;
+
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
   /**
    * TODO: update the state by using Kalman Filter equations
    */
+
+	VectorXd z_pred = H_ * x_;
+	VectorXd y = z - z_pred;
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd K = PHt * Si;
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
    * TODO: update the state by using Extended Kalman Filter equations
+   * This function requires the radar measurement function z = h(x)
    */
+	// z_pred using h(x)
+	VectorXd z_pred(z.size()); // Radar measurement [rho, phi, rho_dot]
+	double px = x_[0];
+	double py = x_[1];
+	double vx = x_[2];
+	double vy = x_[3];
+	double rho = sqrt(px * px + py * py);
+	z_pred[0] = rho;
+	z_pred[1] = atan(py / px);
+	z_pred[2] = (px * vx + py * vy) / rho;
+
+	VectorXd y = z - z_pred;
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd K = PHt * Si;
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
 }
