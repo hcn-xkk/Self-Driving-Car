c35d3ec692bcb858a615cc112e7c41d957a3e2f3
huangchu <huangchu.umich.edu>
2020-07-19 14:17:04 -0400

    Clean up

--- CarND-Extended-Kalman-Filter-Project/src/main.cpp
+++ CarND-Extended-Kalman-Filter-Project/src/main.cpp
@@ -137,9 +137,8 @@ int main() {
           msgJson["rmse_vx"] = RMSE(2);
           msgJson["rmse_vy"] = RMSE(3);
           auto msg = "42[\"estimate_marker\"," + msgJson.dump() + "]";
-          std::cout << msg << std::endl;
+          
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-		  std::cout << "Debug: ws.send in main.cpp done." << std::endl;
         }  // end "telemetry" if
 
       } else {
