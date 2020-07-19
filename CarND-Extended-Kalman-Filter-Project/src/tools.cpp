b695120587e5351b1a58473ccdcbb4d9bc444c8e
huangchu <huangchu.umich.edu>
2020-07-18 21:09:41 -0400

    Update jacobian

--- CarND-Extended-Kalman-Filter-Project/src/tools.cpp
+++ CarND-Extended-Kalman-Filter-Project/src/tools.cpp
@@ -74,8 +74,12 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
 	float rou_3 = rou * rou_2;
 	Hj(0, 0) = px / rou;
 	Hj(0, 1) = py / rou;
+	Hj(0, 2) = 0.0;
+	Hj(0, 3) = 0.0;
 	Hj(1, 0) = -py / rou_2;
 	Hj(1, 1) = px / rou_2;
+	Hj(1, 2) = 0.0;
+	Hj(2, 3) = 0.0;
 	Hj(2, 0) = py * (vx*py - vy * px) / rou_3;
 	Hj(2, 1) = px * (vy*px - vx * py) / rou_3;
 	Hj(2, 2) = px / rou;
