269d11c02cd3bed906906cd9a5e59965c62bafc8
huangchu <huangchu.umich.edu>
2020-08-16 00:06:52 -0400

    Start Path-planning project. Able to follow lane with constant set speed.

---
+++ CarND-Path-Planning-Project-master/src/helpers.h
@@ -0,0 +1,214 @@
+#ifndef HELPERS_H
+#define HELPERS_H
+
+#include <math.h>
+#include <string>
+#include <vector>
+#include <iostream>
+
+// for convenience
+using std::string;
+using std::vector;
+
+// Checks if the SocketIO event has JSON data.
+// If there is data the JSON object in string format will be returned,
+//   else the empty string "" will be returned.
+string hasData(string s) {
+  auto found_null = s.find("null");
+  auto b1 = s.find_first_of("[");
+  auto b2 = s.find_first_of("}");
+  if (found_null != string::npos) {
+    return "";
+  } else if (b1 != string::npos && b2 != string::npos) {
+    return s.substr(b1, b2 - b1 + 2);
+  }
+  return "";
+}
+
+//
+// Helper functions related to waypoints and converting from XY to Frenet
+//   or vice versa
+//
+
+// For converting back and forth between radians and degrees.
+constexpr double pi() { return M_PI; }
+double deg2rad(double x) { return x * pi() / 180; }
+double rad2deg(double x) { return x * 180 / pi(); }
+
+// Calculate distance between two points
+double distance(double x1, double y1, double x2, double y2) {
+  return sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+}
+
+// Calculate closest waypoint to current x, y position
+int ClosestWaypoint(double x, double y, const vector<double> &maps_x, 
+                    const vector<double> &maps_y) {
+  double closestLen = 100000; //large number
+  int closestWaypoint = 0;
+
+  for (int i = 0; i < maps_x.size(); ++i) {
+    double map_x = maps_x[i];
+    double map_y = maps_y[i];
+    double dist = distance(x,y,map_x,map_y);
+    if (dist < closestLen) {
+      closestLen = dist;
+      closestWaypoint = i;
+    }
+  }
+
+  return closestWaypoint;
+}
+
+// Returns next waypoint of the closest waypoint
+int NextWaypoint(double x, double y, double theta, const vector<double> &maps_x, 
+                 const vector<double> &maps_y) {
+  int closestWaypoint = ClosestWaypoint(x,y,maps_x,maps_y);
+
+  double map_x = maps_x[closestWaypoint];
+  double map_y = maps_y[closestWaypoint];
+
+  double heading = atan2((map_y-y),(map_x-x));
+
+  double angle = fabs(theta-heading);
+  angle = std::min(2*pi() - angle, angle);
+
+  if (angle > pi()/2) {
+    ++closestWaypoint;
+    if (closestWaypoint == maps_x.size()) {
+      closestWaypoint = 0;
+    }
+  }
+
+  return closestWaypoint;
+}
+
+// Transform from Cartesian x,y coordinates to Frenet s,d coordinates
+vector<double> getFrenet(double x, double y, double theta, 
+                         const vector<double> &maps_x, 
+                         const vector<double> &maps_y) {
+  int next_wp = NextWaypoint(x,y, theta, maps_x,maps_y);
+
+  int prev_wp;
+  prev_wp = next_wp-1;
+  if (next_wp == 0) {
+    prev_wp  = maps_x.size()-1;
+  }
+
+  double n_x = maps_x[next_wp]-maps_x[prev_wp];
+  double n_y = maps_y[next_wp]-maps_y[prev_wp];
+  double x_x = x - maps_x[prev_wp];
+  double x_y = y - maps_y[prev_wp];
+
+  // find the projection of x onto n
+  double proj_norm = (x_x*n_x+x_y*n_y)/(n_x*n_x+n_y*n_y);
+  double proj_x = proj_norm*n_x;
+  double proj_y = proj_norm*n_y;
+
+  double frenet_d = distance(x_x,x_y,proj_x,proj_y);
+
+  //see if d value is positive or negative by comparing it to a center point
+  double center_x = 1000-maps_x[prev_wp];
+  double center_y = 2000-maps_y[prev_wp];
+  double centerToPos = distance(center_x,center_y,x_x,x_y);
+  double centerToRef = distance(center_x,center_y,proj_x,proj_y);
+
+  if (centerToPos <= centerToRef) {
+    frenet_d *= -1;
+  }
+
+  // calculate s value
+  double frenet_s = 0;
+  for (int i = 0; i < prev_wp; ++i) {
+    frenet_s += distance(maps_x[i],maps_y[i],maps_x[i+1],maps_y[i+1]);
+  }
+
+  frenet_s += distance(0,0,proj_x,proj_y);
+
+  return {frenet_s,frenet_d};
+}
+
+// Transform from Frenet s,d coordinates to Cartesian x,y
+vector<double> getXY(double s, double d, const vector<double> &maps_s, 
+                     const vector<double> &maps_x, 
+                     const vector<double> &maps_y) {
+  int prev_wp = -1;
+
+  while (s > maps_s[prev_wp+1] && (prev_wp < (int)(maps_s.size()-1))) {
+    ++prev_wp;
+  }
+
+  int wp2 = (prev_wp+1)%maps_x.size();
+
+  double heading = atan2((maps_y[wp2]-maps_y[prev_wp]),
+                         (maps_x[wp2]-maps_x[prev_wp]));
+  // the x,y,s along the segment
+  double seg_s = (s-maps_s[prev_wp]);
+
+  double seg_x = maps_x[prev_wp]+seg_s*cos(heading);
+  double seg_y = maps_y[prev_wp]+seg_s*sin(heading);
+
+  double perp_heading = heading-pi()/2;
+
+  double x = seg_x + d*cos(perp_heading);
+  double y = seg_y + d*sin(perp_heading);
+
+  return {x,y};
+}
+
+vector<vector<double>> SE2Transform(const vector<double> &x_R1, const vector<double> &y_R1,
+	const double &xO1_R2, const double &yO1_R2, const double &theta1_R2) {
+	// Transforming a series of points from 1 to 2:
+	// xO1_R2, yO1_R2 are the x,y coords of O1 represented in (X2,Y2)
+	// theta1_R2 is the rotation angle of (X1,Y1) relative to (X2,Y2)
+	vector<double> x_R2, y_R2;
+	int length = x_R1.size();
+	double x1, y1;
+	for (int i = 0; i < length; i++) {
+		x1 = x_R1[i];
+		y1 = y_R1[i];
+		x_R2.push_back(cos(theta1_R2) * x1 - sin(theta1_R2) * y1 + xO1_R2);
+		y_R2.push_back(sin(theta1_R2) * x1 + cos(theta1_R2) * y1 + yO1_R2);
+	}
+	return { x_R2, y_R2 };
+}
+vector<double> SE2Transform(const double &x_R1, const double &y_R1,
+	const double &xO1_R2, const double &yO1_R2, const double &theta1_R2) {
+	// Transforming one point from 1 to 2:
+	// xO1_R2, yO1_R2 are the x,y coords of O1 represented in (X2,Y2)
+	// theta1_R2 is the rotation angle of (X1,Y1) relative to (X2,Y2).
+	// R1|2 = [[x1|2,y1|2],[O1|2;1]];
+	// v|2 = R1|0 * v|1
+	double x_R2, y_R2;
+	x_R2 = (cos(theta1_R2) * x_R1 - sin(theta1_R2) * y_R1 + xO1_R2);
+	y_R2 = (sin(theta1_R2) * x_R1 + cos(theta1_R2) * y_R1 + yO1_R2);
+	return { x_R2, y_R2 };
+}
+
+vector<vector<double>> GlobalToCarTransform(const vector<double> &x_g, const vector<double> &y_g,
+	const double &xCar_g, const double &yCar_g, const double &heading) {
+	// Know global coordinates x_g, y_g, want to get car coordinates x_car, y_car
+	// xCar_g, yCar_g, heading are place and angle of car in global coordinates
+	double xOGlobal_car = -(xCar_g * cos(heading) + yCar_g * sin(heading));
+	double yOGlobal_car = -(yCar_g * cos(heading) - xCar_g * sin(heading));
+	double theta = -heading;
+	return SE2Transform(x_g, y_g, xOGlobal_car, yOGlobal_car, theta);
+}
+vector<double> GlobalToCarTransform(const double &x_g, const double &y_g,
+	const double &xCar_g, const double &yCar_g, const double &heading) {
+	// Know global coordinates x_g, y_g, want to get car coordinates x_car, y_car
+	// xCar_g, yCar_g, heading are place and angle of car in global coordinates
+	double xOGlobal_car = -(xCar_g * cos(heading) + yCar_g * sin(heading));
+	double yOGlobal_car = -(yCar_g * cos(heading) - xCar_g * sin(heading));
+	double theta = -heading;
+	return SE2Transform(x_g, y_g, xOGlobal_car, yOGlobal_car, theta);
+}
+
+void printVector(vector<double> v) {
+	for (int i = 0; i < v.size(); i++) {
+		std::cout << v[i] << ' ';
+	}
+	std::cout << ' ' << std::endl;
+}
+
+
+#endif  // HELPERS_H
