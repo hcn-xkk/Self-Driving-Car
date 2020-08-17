980f25d9f62592d0199bb05eb7cc338e9d672f30
huangchu <huangchu.umich.edu>
2020-08-17 00:55:36 -0400

    Debug. Collision avoidance

--- CarND-Path-Planning-Project-master/src/helpers.h
+++ CarND-Path-Planning-Project-master/src/helpers.h
@@ -5,6 +5,13 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <tuple>
+#include <set>
+#include "Eigen-3.3/Eigen/Dense"
+
+using std::vector;
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
 
 // for convenience
 using std::string;
@@ -208,6 +215,269 @@ void printVector(vector<double> v) {
 		std::cout << v[i] << ' ';
 	}
 	std::cout << ' ' << std::endl;
+}
+
+
+int checkLaneEmpty(int lane_id, vector<vector<double>> lane_id_s_list, vector<vector<double>> sensor_fusion, double max_map_s) {
+	int output = 0;
+
+	for (int j = 0; j < lane_id_s_list.size(); j++) {
+		double s_start = lane_id_s_list[j][0];
+		double s_end = lane_id_s_list[j][1];
+
+		for (int i = 0; i < sensor_fusion.size(); i++) {
+			if (lane_id == sensor_fusion[i][6]) {
+				if ((sensor_fusion[i][5] >= s_start) && (sensor_fusion[i][5] < s_end)) {
+					output += 1;
+					return output;
+				}
+				if ((sensor_fusion[i][5] + max_map_s >= s_start) && (sensor_fusion[i][5] + max_map_s < s_end)) {
+					output += 1;
+					return output;
+				}
+			}
+		}
+	}
+	return output;
+}
+
+inline int getLaneId(double d, double yellow_line_d, double lane_width) {
+	return (int)floor((d - yellow_line_d) / lane_width);
+}
+
+inline double calculatePolynomial(vector<double> c, double t) {
+	double y = 0.0;
+	for (int i = 0; i < c.size(); i++) {
+		y += c[i] * pow(t, i);
+	}
+	return y;
+}
+vector<double> JMT(vector<double> &start, vector<double> &end, double T_start, double T_end) {
+	/**
+	 * Calculate the Jerk Minimizing Trajectory that connects the initial state
+	 * to the final state in time T.
+	 *
+	 * @param start - the vehicles start location given as a length three array
+	 *   corresponding to initial values of [s, s_dot, s_double_dot]
+	 * @param end - the desired end state for vehicle. Like "start" this is a
+	 *   length three array.
+	 * @param T - The duration, in seconds, over which this maneuver should occur.
+	 *
+	 * @output an array of length 6, each value corresponding to a coefficent in
+	 *   the polynomial:
+	 *   s(t) = a_0 + a_1 * t + a_2 * t**2 + a_3 * t**3 + a_4 * t**4 + a_5 * t**5
+	 *
+	 * EXAMPLE
+	 *   > JMT([0, 10, 0], [10, 10, 0], 1)
+	 *     [0.0, 10.0, 0.0, 0.0, 0.0, 0.0]
+	 */
+	double T = T_end - T_start;
+	// parameters a0~a5: time span [0,T]
+	double a0 = start[0];
+	double a1 = start[1];
+	double a2 = start[2] / 2.0;
+	MatrixXd A(3, 3);
+	A << std::pow(T, 3), std::pow(T, 4), std::pow(T, 5),
+		3 * std::pow(T, 2), 4 * std::pow(T, 3), 5 * std::pow(T, 4),
+		6 * T, 12 * std::pow(T, 2), 20 * std::pow(T, 3);
+	VectorXd b(3);
+	b << end[0] - (start[0] + start[1] * T + 0.5*start[2] * T*T),
+		end[1] - (start[1] + start[2] * T),
+		end[2] - start[2];
+
+	VectorXd a345 = A.colPivHouseholderQr().solve(b);
+
+	double a3 = a345[0];
+	double a4 = a345[1];
+	double a5 = a345[2];
+
+	// parameters c0~c5: time span [T_start, T_end]
+	double T_start2 = T_start * T_start;
+	double T_start3 = T_start * T_start2;
+	double T_start4 = T_start * T_start3;
+	double T_start5 = T_start * T_start4;
+
+	double c0 = a0 - a1 * T_start + a2 * T_start2 - a3 * T_start3 + a4 * T_start4 - a5 * T_start5;
+	double c1 = a1 - a2 * T_start * 2 + a3 * 3 * T_start2 - a4 * 4 * T_start3 + a5 * 5 * T_start4;
+	double c2 = a2 - a3 * 3 * T_start + a4 * 6 * T_start2 - a5 * 10 * T_start3;
+	double c3 = a3 - a4 * 4 * T_start + a5 * 10 * T_start2;
+	double c4 = a4 - a5 * 5 * T_start;
+	double c5 = a5;
+
+	return { c0,c1,c2,c3,c4,c5 };
+}
+
+
+
+std::tuple<vector<int>, vector<vector<double>> > getRegionToTravel(double end_previous_path_s, double end_previous_path_d, 
+	double car_s, double car_d, double set_speed, int N, int previous_length, double max_map_s, 
+	double yellow_line_d, double lane_width) {
+	// Generate the list of lane_id(s) and list of road segments in s (Frenet) coordinate
+	// in T=1second horizon, at most one lane change is planned. 
+	// If car_d == end_path_d, then there is not lane change.
+	// Otherwise, there is lane change.
+	vector<int> planned_lane_id_list;
+	vector<vector<double>> planned_lane_s_list;
+	if (true) { // && (getLaneId(car_d, yellow_line_d, lane_width) == getLaneId(end_path_d, yellow_line_d, lane_width))) { 
+		// case with no lane change
+		double end_path_s = end_previous_path_s + set_speed * (N - previous_length);
+		if (end_path_s <= max_map_s) { // no looping the cycle happening.
+			planned_lane_id_list.push_back(getLaneId(car_d, yellow_line_d, lane_width));
+			planned_lane_s_list.push_back({ car_s, end_path_s });
+		}
+		else if (car_s <= end_previous_path_s) { // loop the cycle when extending the plan
+			planned_lane_id_list.push_back(getLaneId(car_d, yellow_line_d, lane_width));
+			planned_lane_id_list.push_back(getLaneId(car_d, yellow_line_d, lane_width));
+			planned_lane_s_list.push_back({ car_s, max_map_s });
+			planned_lane_s_list.push_back({ 0, fmod(end_path_s, max_map_s) });
+		}
+		else { // loop the cycle in the previous planned path
+			planned_lane_id_list.push_back(getLaneId(car_d, yellow_line_d, lane_width));
+			planned_lane_id_list.push_back(getLaneId(car_d, yellow_line_d, lane_width));
+			planned_lane_s_list.push_back({ car_s, max_map_s });
+			planned_lane_s_list.push_back({ 0, fmod(end_path_s, max_map_s) });
+		}
+	}
+	else {// it there is lane change
+	}
+	return std::make_tuple(planned_lane_id_list, planned_lane_s_list);
+}
+
+
+std::tuple<int, vector<double>> planBehaviorAndTraj(vector<int> planned_lane_id_list,
+	vector<vector<double>> planned_lane_s_list, vector<vector<double>> sensor_fusion, 
+	double T, double dT, int previous_length, 
+	vector<double> previous_path_x, vector<double> previous_path_y, double end_path_s, double end_path_d,
+	double set_speed, double max_map_s,
+	double car_s, double car_d, double car_speed, 
+	double yellow_lane_d, double lane_width) {
+	// mode: 
+	// [0]: keep lane, re-use previous traj and track set_speed. {T_planned,T_end,s_f,ds_f,dds_f}. 
+	// [1]: planned keep lane, generate new trajectory. Output minimum jerk traj {T_end,s_f,ds_f,dds_f},T_planned=0.
+	// [2]: lane change left, re-use previous traj. Output minimum jerk traj {T_planned,T_end,s_f,ds_f,dds_f,d_f,d(d)_f,dd(d)_f}
+	// [3]: planned lane change left, generate new traj. Output minimum jerk traj {T_end,s_f,ds_f,dds_f,d_f,d(d)_f,dd(d)_f},T_planned=0.
+	// [4]: lane change right, re-use previous traj. Output minimum jerk traj {T_planned,T_end,s_f,ds_f,dds_f,d_f,d(d)_f,dd(d)_f}
+	// [5]: planned lane change right, generate new traj. Output minimum jerk traj {T_end,s_f,ds_f,dds_f,d_f,d(d)_f,dd(d)_f},T_planned=0.
+	// [6]: no planned trajectory. 
+
+	// Calculate behavior_mode:
+	// Plan new trajectory if there is other car in planned_lane_s_list
+	// bool b_lane_change_planned = (std::set<int>(planned_lane_id_list.begin(), planned_lane_id_list.end()).size() > 1);
+	int behavior_mode;
+	int lane_id2;
+	// max_map_s = 6945.554;
+	if (previous_length >= 3) {
+		if (*planned_lane_id_list.begin() == *(planned_lane_id_list.end() - 1)) { // mode 0 or 1.
+			// check if there is vehicle inside (d,s) range
+			behavior_mode = 0; // +checkLaneEmpty(*planned_lane_id_list.begin(), planned_lane_s_list, sensor_fusion);
+			lane_id2 = *planned_lane_id_list.begin();
+		}
+		else if (*planned_lane_id_list.begin() > *(planned_lane_id_list.end() - 1)) { // mode 2 or 3
+			int lane_id1 = *planned_lane_id_list.begin();
+			lane_id2 = *(planned_lane_id_list.end() - 1);
+			vector<vector<double>> lane_id1_s_list, lane_id2_s_list;
+			for (int i = 0; i < planned_lane_id_list.size(); i++) {
+				if (planned_lane_id_list[i] == lane_id1) {
+					lane_id1_s_list.push_back(planned_lane_s_list[i]);
+				}
+				else {
+					lane_id2_s_list.push_back(planned_lane_s_list[i]);
+				}
+			}
+			behavior_mode = 2 + std::max(checkLaneEmpty(lane_id1, lane_id1_s_list, sensor_fusion, max_map_s),
+				checkLaneEmpty(lane_id2, lane_id2_s_list, sensor_fusion, max_map_s));
+		}
+		else { // mode 4 or 5
+			int lane_id1 = *planned_lane_id_list.begin();
+			lane_id2 = *(planned_lane_id_list.end() - 1);
+			vector<vector<double>> lane_id1_s_list, lane_id2_s_list;
+			for (int i = 0; i < planned_lane_id_list.size(); i++) {
+				if (planned_lane_id_list[i] == lane_id1) {
+					lane_id1_s_list.push_back(planned_lane_s_list[i]);
+				}
+				else {
+					lane_id2_s_list.push_back(planned_lane_s_list[i]);
+				}
+			}
+			behavior_mode = 4 + std::max(checkLaneEmpty(lane_id1, lane_id1_s_list, sensor_fusion, max_map_s),
+				checkLaneEmpty(lane_id2, lane_id2_s_list, sensor_fusion, max_map_s));
+		}
+	}
+	else {
+		behavior_mode = 6;
+	}
+
+
+	// Generate totally new trajectory or continuing trajectory based on mode:
+	vector<double> behavior_parameters;
+	if (behavior_mode == 0 || behavior_mode == 2 || behavior_mode == 4) {
+		double T_planned = previous_length * dT;
+		double T_end = T;
+		double s_start = end_path_s;
+		double d_s_start = sqrt(pow((previous_path_x[previous_length-1]- previous_path_x[previous_length - 2])/dT,2) + 
+			pow((previous_path_y[previous_length - 1] - previous_path_y[previous_length - 2]) / dT, 2));
+		double d_s_second_last = sqrt(pow((previous_path_x[previous_length - 2] - previous_path_x[previous_length - 3]) / dT, 2) +
+			pow((previous_path_y[previous_length - 2] - previous_path_y[previous_length - 3]) / dT, 2));
+		double dd_s_start = (d_s_start - d_s_second_last) / dT;
+		double s_end = end_path_s + (T_end - T_planned) * set_speed;
+		double d_s_end = set_speed;
+		double dd_s_end = 0.0;
+		std::cout << "original s_end is " << s_end << std::endl;
+		// look for preceding vehicle and track
+		for (int i = 0; i < sensor_fusion.size(); i++) {
+			if (lane_id2 == sensor_fusion[i][6]) {
+				if ((sensor_fusion[i][5] >= s_start) && (sensor_fusion[i][5] < s_end)) {
+					s_end = sensor_fusion[i][5];
+					d_s_end = sqrt(pow(sensor_fusion[i][3], 2) + pow(sensor_fusion[i][4], 2));
+				}
+				if ((sensor_fusion[i][5] + max_map_s >= s_start) && (sensor_fusion[i][5] + max_map_s < s_end)) {
+					s_end = sensor_fusion[i][5] + max_map_s;
+					d_s_end = sqrt(pow(sensor_fusion[i][3], 2) + pow(sensor_fusion[i][4], 2));
+				}
+			}
+		}
+		std::cout << "modified s_end is " << s_end << std::endl;
+		// Polynomial coefficients
+		vector<double> start{ s_start,d_s_start,dd_s_start };
+		vector<double> end{ s_end,d_s_end,dd_s_end };
+
+		behavior_parameters = JMT(start, end, T_planned, T_end);
+
+		std::cout << "T_planned is " << T_planned << std::endl;
+		printVector(behavior_parameters);
+	}
+	else { // plan a new trajectory following lane
+
+		double T_planned = 0;
+		double T_end = T;
+		double s_start = car_s;
+		double d_s_start = car_speed;
+		double dd_s_start = 0.0;
+		double s_end = car_s + (T_end - T_planned) * set_speed;
+		double d_s_end = set_speed;
+		double dd_s_end = 0.0;
+		lane_id2 = getLaneId(car_d, yellow_lane_d, lane_width);
+		// look for preceding vehicle and track
+		for (int i = 0; i < sensor_fusion.size(); i++) {
+			if (lane_id2 == sensor_fusion[i][6]) {
+				if ((sensor_fusion[i][5] >= s_start) && (sensor_fusion[i][5] < s_end)) {
+					s_end = sensor_fusion[i][5];
+					d_s_end = sqrt(pow(sensor_fusion[i][3], 2) + pow(sensor_fusion[i][4], 2));
+				}
+				if ((sensor_fusion[i][5] + max_map_s >= s_start) && (sensor_fusion[i][5] + max_map_s < s_end)) {
+					s_end = sensor_fusion[i][5] + max_map_s;
+					d_s_end = sqrt(pow(sensor_fusion[i][3], 2) + pow(sensor_fusion[i][4], 2));
+				}
+			}
+		}
+		// Polynomial coefficients
+		vector<double> start{ s_start,d_s_start,dd_s_start };
+		vector<double> end{ s_end,d_s_end,dd_s_end };
+
+		behavior_parameters = JMT(start, end, T_planned, T_end);
+	}
+
+	return std::make_tuple(behavior_mode, behavior_parameters);
 }
 
 
