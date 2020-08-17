10561ed65d418ceb34854c5d0bc96f08c282afde
huangchu <huangchu.umich.edu>
2020-08-17 19:27:28 -0400

    Debug and clean code for ACC

--- CarND-Path-Planning-Project-master/src/main.cpp
+++ CarND-Path-Planning-Project-master/src/main.cpp
@@ -156,7 +156,7 @@ int main() {
 					// Set acceleration / deceleration for generating future waypoints.
 					double set_accel = 5;
 
-					double speed_increment = set_accel * (0.5*T);  // 
+					double speed_increment = set_accel * (0.2*T);  // 
 					double k_accel;
 					if (ref_speed > set_speed + speed_increment) {
 						ref_speed -= speed_increment; // using -5m/s^2 accel
