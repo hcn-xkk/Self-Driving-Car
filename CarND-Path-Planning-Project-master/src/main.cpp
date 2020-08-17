b1b94b3913ec31d2531ff702f163a02daf093155
huangchu <huangchu.umich.edu>
2020-08-17 14:32:46 -0400

    Debug collision

--- CarND-Path-Planning-Project-master/src/main.cpp
+++ CarND-Path-Planning-Project-master/src/main.cpp
@@ -138,14 +138,16 @@ int main() {
 					*/
 					int lane_is_ocupied = 0;
 					for (int i = 0; i < sensor_fusion.size(); i++) {
-						if (lane_id == getLaneId(sensor_fusion[i][6], yellow_line_d, lane_width)) {
+						double check_d = sensor_fusion[i][6];
+						if (lane_id == getLaneId(check_d, yellow_line_d, lane_width)) {
 							double vx = sensor_fusion[i][3];
 							double vy = sensor_fusion[i][4];
 							check_speed = sqrt(pow(vx, 2) + pow(vy, 2));
 							double prev_check_car_s = sensor_fusion[i][5];
 							check_car_s = prev_check_car_s + dT * check_speed;
-							if (((check_car_s >= car_s) && (check_car_s < car_s + set_speed * T))) {
+							if (((check_car_s >= car_s) && (check_car_s < car_s + set_speed*10 * T))) {
 								lane_is_ocupied = 1;
+								break;
 							}
 						}
 					}
